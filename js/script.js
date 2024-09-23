b077f85c3914d726b9bf22cce5bb0c53e3acc0ee
bahaa bebo <bahaabebo1122@gmail.com>
2024-09-24 02:58:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+(function ($){
+    "use strict"
+    //Spinner
+    var spinner = function () {
+        setTimeout(function(){
+            if($('#spinner').length > 0 ){
+                $('#spinner').removeClass('show');
+            }
+        },1)
+    };
+    spinner();
+    //wow js
+    new WOW().init();
+    //navbar
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('.sticky-top').addClass('shadow-sm').css('top','0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top','-100px');
+        }
+    });
+
+})(jQuery)
